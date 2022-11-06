68c76ea27a9ffcf4c28108fc152379a78666acb0
livid98 <liviad877@gmail.com>
2022-11-06 17:11:16 -0300

    formatado

--- index.js
+++ index.js
@@ -10,33 +10,33 @@ const users = [];
 const tweets = [];
 
 
-app.post("/sign-up", (req,res) => {
- const {username, tweet} = req.body;
- users.push(req.body);
- console.log(users);
- res.send("Ok")
+app.post("/sign-up", (req, res) => {
+   const { username, tweet } = req.body;
+   users.push(req.body);
+   console.log(users);
+   res.send("Ok")
 
 });
 
 
-app.post("/tweets", (req,res) => {
-   const {username, tweet} = req.body;
-   
+app.post("/tweets", (req, res) => {
+   const { username, tweet } = req.body;
+
    const newTweet = {
       username,
-      avatar: users.find((user)=> user.username === username).avatar,
+      avatar: users.find((user) => user.username === username).avatar,
       tweet
-    };
+   };
 
    tweets.push(newTweet);
    res.send("Ok")
 });
 
-app.get("/tweets", (req,res) => {
- const lastTweets = tweets.slice(-10);
- res.send(lastTweets);
+app.get("/tweets", (req, res) => {
+   const lastTweets = tweets.slice(-10);
+   res.send(lastTweets);
 });
 
 app.listen(5000, () => {
-console.log("App running on port 5000")
+   console.log("App running on port 5000")
 })
